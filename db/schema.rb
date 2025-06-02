6af7513af834f0444796ed5bc1d7a3da989fd810
Thomas Bruins <thomas.bruins12@gmail.com>
2025-06-02 15:56:17 +0200

    Created the models

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_02_131742) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_02_135551) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "quests", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "xp_granted"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_classes", force: :cascade do |t|
+    t.integer "xp"
+    t.integer "level"
+    t.string "type"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_classes_on_user_id"
+  end
+
+  create_table "user_quests", force: :cascade do |t|
+    t.bigint "quest_id", null: false
+    t.bigint "user_id", null: false
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["quest_id"], name: "index_user_quests_on_quest_id"
+    t.index ["user_id"], name: "index_user_quests_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,8 +51,16 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_02_131742) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_birth"
+    t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "user_classes", "users"
+  add_foreign_key "user_quests", "quests"
+  add_foreign_key "user_quests", "users"
 end
