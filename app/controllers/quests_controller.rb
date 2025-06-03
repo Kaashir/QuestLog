98dce44dc341be8884876003888a77e7f786e3bc
Carlijn Driessen <carlijndriessen.dev@gmail.com>
2025-06-03 16:36:21 +0200

    user_quests controller added

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,4 @@
-class QuestController < ApplicationController
+class QuestsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_quest, only: [:show, :edit, :update, :destroy]
 
