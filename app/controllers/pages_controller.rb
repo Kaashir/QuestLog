bfc20f1ba6b046db16db7a25d266f5ed6c43a2c3
Mrinal Verma <mrinalverma1993@gmail.com>
2025-06-04 19:40:33 +0200

    Saved Temporarily

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home] # Skip authentication for the home action
+  # This allows the home page to be accessible without requiring a user to be logged in.
 
   def home
   end
+
 end
