3044f4c869c9f9ef64201b6b516cf08b84d1480e
Thomas Bruins <thomas.bruins12@gmail.com>
2025-06-11 15:24:30 +0200

    idk

--- app/models/user_quest.rb
+++ app/models/user_quest.rb
@@ -18,6 +18,7 @@ class UserQuest < ApplicationRecord
   scope :for_hero_class, lambda { |hero_class|
     joins(:quest)
       .where(quests: { quest_category: hero_class.quest_categories })
+    }
 
   private
 
